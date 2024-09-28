6ef00722f6aa2864ff7395f496a8078795d08eba
Migs <zjm0154@gmail.com>
2024-09-28 20:36:41 +0800

    test

---
+++ app/src/main/java/com/migsdev/tastytrends/ForgotPassActivity.kt
@@ -0,0 +1,28 @@
+package com.migsdev.tastytrends
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ForgotPassActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_forgot_pass)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnreturn = findViewById<Button>(R.id.btnreturn)
+        btnreturn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/migsdev/tastytrends/LoginActivity.kt
@@ -0,0 +1,35 @@
+package com.migsdev.tastytrends
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnsignup = findViewById<Button>(R.id.btnreturn)
+        btnsignup.setOnClickListener {
+            val intent = Intent(this, SigUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        val tvforgotbtn = findViewById<TextView>(R.id.tvforgotbtn)
+        tvforgotbtn.setOnClickListener {
+            val intent = Intent(this, ForgotPassActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
